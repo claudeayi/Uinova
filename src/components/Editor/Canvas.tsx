66c1464c14a55989b6b2201ac31875d03d5cac03
claudeayi <claudejordanayissi@gmail.com>
2025-08-07 20:16:03 +0100

    Create src/components/Editor/Canvas.tsx

---
+++ src/components/Editor/Canvas.tsx
@@ -0,0 +1,7 @@
+export default function Canvas() {
+  return (
+    <div className="border border-dashed border-gray-400 p-10 min-h-[300px] rounded-lg bg-white shadow-md">
+      Drop your components here.
+    </div>
+  );
+}
