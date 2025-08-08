45e55dbbb8f2f54dcf5321af15c44c9a37549ae5
claudeayi <claudejordanayissi@gmail.com>
2025-08-08 20:03:49 +0100

    Create src/themes/theme.ts

---
+++ src/themes/theme.ts
@@ -0,0 +1,10 @@
+export const lightTheme = {
+  background: "#F7F8FA",
+  foreground: "#222",
+  accent: "#3841f7",
+};
+export const darkTheme = {
+  background: "#181A22",
+  foreground: "#FFF",
+  accent: "#E2E8FF",
+};
