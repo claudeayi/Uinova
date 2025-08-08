659203185022cafd5d3f9bcbcea5864fdd28650a
claudeayi <claudejordanayissi@gmail.com>
2025-08-08 20:16:23 +0100

    Create src/hooks/useToast.ts

---
+++ src/hooks/useToast.ts
@@ -0,0 +1,8 @@
+import { toast } from "react-hot-toast";
+export default function useToast() {
+  return {
+    success: (msg: string) => toast.success(msg),
+    error: (msg: string) => toast.error(msg),
+    info: (msg: string) => toast(msg)
+  };
+}
