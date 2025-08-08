78ea18ba304651f0af664a78c4bdb858586c1a94
claudeayi <claudejordanayissi@gmail.com>
2025-08-08 04:47:21 +0100

    Create src/themes/palette.ts

---
+++ src/themes/palette.ts
@@ -0,0 +1,14 @@
+export const UInovaPalette = {
+  light: {
+    primary: "#2362f7",
+    secondary: "#4ad991",
+    background: "#f4f4f5",
+    text: "#18181b",
+  },
+  dark: {
+    primary: "#6ba0fa",
+    secondary: "#3fd37d",
+    background: "#18181b",
+    text: "#f4f4f5",
+  },
+};
