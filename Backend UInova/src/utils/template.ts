b03c6b885cb138dff74ed9a3bf8460395dfe3cd9
claudeayi <claudejordanayissi@gmail.com>
2025-08-08 07:11:24 +0100

    Create src/utils/template.ts

---
+++ Backend UInova/src/utils/template.ts
@@ -0,0 +1,6 @@
+export function renderWelcomeMail(userName: string) {
+  return `
+    <h1>Bienvenue sur UInova, ${userName} !</h1>
+    <p>Merci de rejoindre la plateforme nocode la plus avancée.</p>
+  `;
+}
