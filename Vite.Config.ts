e677aaec5dba6b0ce1b726476d3c47fb165caa45
claudeayi <claudejordanayissi@gmail.com>
2025-08-10 17:05:47 +0100

    Update Vite.Config.ts

--- Vite.Config.ts
+++ Vite.Config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 5173, // ou un autre port si besoin
+    open: true, // ouvre le navigateur au démarrage
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true
+  },
+});
