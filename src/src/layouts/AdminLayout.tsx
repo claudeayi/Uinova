24152068c56ba87d1a2b8ddfb798ddd2d35a3406
claudeayi <claudejordanayissi@gmail.com>
2025-09-03 00:53:40 +0100

    Update AdminLayout.tsx

--- src/src/layouts/AdminLayout.tsx
+++ src/src/layouts/AdminLayout.tsx
@@ -1,13 +1,23 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "@/components/admin/AdminSidebar";
+import AdminTopbar from "@/components/admin/AdminTopbar";
 
 export default function AdminLayout() {
   return (
     <div className="flex h-screen">
+      {/* Sidebar */}
       <AdminSidebar />
-      <main className="flex-1 overflow-y-auto bg-gray-50 dark:bg-slate-950">
-        <Outlet />
-      </main>
+
+      {/* Main content */}
+      <div className="flex flex-col flex-1">
+        {/* Topbar */}
+        <AdminTopbar />
+
+        {/* Contenu */}
+        <main className="flex-1 overflow-y-auto bg-gray-50 dark:bg-slate-950">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
