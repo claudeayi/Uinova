d509a02f5795e7c123540f1b467693e99d79a7b4
claudeayi <claudejordanayissi@gmail.com>
2025-09-07 10:34:05 +0100

    Delete Backend UInova/routes/templates.ts

--- Backend UInova/routes/templates.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { listTemplates, getTemplate } from "../controllers/templateController";
-
-const router = Router();
-
-router.get("/", listTemplates);
-router.get("/:id", getTemplate);
-
-export default router;
